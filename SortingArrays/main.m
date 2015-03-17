08faf0b7b21bd5ae67c13055d41e61345d4ec6a0
zak123 <zacharym123@gmail.com>
2015-03-17 11:32:34 -0700

    Initial Commit

---
+++ SortingArrays/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SortingArrays
+//
+//  Created by Zachary Mallicoat on 3/17/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
